150c7044a3f328479a88bf01058bc061747cf88c
Oscar Anguaino Gonzalez <95459275+Oscar060502@users.noreply.github.com>
2024-12-12 16:51:15 -0800

    Update problema1.py

--- Problema-1/problema1.py
+++ Problema-1/problema1.py
@@ -1,13 +1,23 @@
-numbers = [2,3,4,2,5,5,4,3,5,4]
+numbers = [2,3,4,2,5,5,4,3,5,4,2,4]
+
+PRIMERA_CARRERA_X = numbers[0] + numbers[1]
+PRIMERA_CARRERA_Y = numbers[2] + numbers[3]
 
 for x in numbers:
-    carroX = numbers[x] + numbers[x+1]
-    carroY = numbers[x+2] + numbers[x+3]
-    x = 0
-print(carroX)
-print(carroY)
+    SEGUNDA_CARRERA_X = numbers[x] + numbers[x+1]
+    SEGUNDA_CARRERA_Y = numbers[x+2] + numbers[x+3]
 
-if carroX > carroY:
+print(PRIMERA_CARRERA_X)
+print(PRIMERA_CARRERA_Y)
+print(SEGUNDA_CARRERA_X)
+print(SEGUNDA_CARRERA_Y)
+
+if PRIMERA_CARRERA_X  > PRIMERA_CARRERA_Y:
+    print("Carro X ganador")
+else:
+    print("Carro Y ganador")
+
+if SEGUNDA_CARRERA_X  > SEGUNDA_CARRERA_Y:
     print("Carro X ganador")
 else:
     print("Carro Y ganador")
